63056475673bbff171238be931d5d16f46390d07
Aleksandr Oleshko <oleshko_o@dlit.dp.ua>
2021-02-17 20:40:19 +0200

    complete files array

--- src/constants/boardConstants.js
+++ src/constants/boardConstants.js
@@ -2,7 +2,7 @@ export const ROWS_LENGTH = 8;
 export const COLUMNS_LENGTH = 8;
 
 export const RANKS_ARRAY = [1, 2, 3, 4, 5, 6, 7, 8];
-export const FILES_ARRAY = ["a", "b", "c", "d", "e", "f", "g"];
+export const FILES_ARRAY = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
 export const RANKS_LEFT_SIDE = "left";
 export const RANKS_RIGHT_SIDE = "right";
